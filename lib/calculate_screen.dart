6f3d7f868e23423aa89c7ac835e51431a74c97a5
Kulubekov Alisher <kulubekovalish@icloud.com>
2025-03-09 11:26:50 +0600

    Initial commit

---
+++ lib/calculate_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class CalculateScreen extends StatefulWidget {
+  const CalculateScreen({super.key});
+
+  @override
+  State<CalculateScreen> createState() => _CalculateScreenState();
+}
+
+class _CalculateScreenState extends State<CalculateScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:calculate_neumorphism/calculate_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CalculateScreen());
+}
