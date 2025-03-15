9ebfb8f9666e7783f05b886dab36d81b10801e56
Kulubekov Alisher <kulubekovalish@icloud.com>
2025-03-15 17:56:43 +0600

    Home Work 1

---
+++ lib/my_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class myColors{
+  static const  Color backgroundColor = Color(0x252B38);
+  static const  Color linearGrey = Color(0x545F71);
+  static const  Color linearDarkGrey = Color(0x252B38);
+  static const  Color orangeLikePH = Color(0xD9781F);
+}
